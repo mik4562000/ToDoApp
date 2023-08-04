fc2a901c947c13afd6323be94d4ccd2b91cca33d
mik4562000 <donkey83@bk.ru>
2023-08-04 12:54:41 +0300

    Added tests for controller and service

--- ToDoListManagement.Tests/Controllers/ToDoControllerTests.cs
+++ ToDoListManagement.Tests/Controllers/ToDoControllerTests.cs
@@ -1,76 +1,228 @@
-using Microsoft.Extensions.Logging;
-using Microsoft.VisualStudio.TestPlatform.Common.Exceptions;
 using Moq;
 using toDoListManagement.Controllers;
 using toDoListManagement.Models;
-using toDoListManagement.Services;
 using toDoListManagement.Services.Contracts;
 
 namespace ToDoListManagement.Tests.Controllers
 {
     public class ToDoControllerTests
     {
-        private readonly Mock<IToDoService> _mockService;
+        private readonly ToDoController _controller;
+        private readonly Mock<IToDoService> _toDoServiceMock;
 
         public ToDoControllerTests() 
         {
-            _mockService = new Mock<IToDoService>();
+            _toDoServiceMock = new Mock<IToDoService>();
+            _controller = new ToDoController(_toDoServiceMock.Object);
         }
+
         [Fact]
-        public void GetAllToDoItems_shouldReturnIEnumerableToDoItem()
+        public void GetAllToDoItems_ShouldCallServiceMethodAndReturnExpectedList()
         {
-            //arrange
-            var stubList = new[]
+            //Arrange
+            var expectedToDoList = new[]
             {
                 new ToDoItem
                 {
                     Id = 1,
-                    Name = "Foo",
+                    Name = "Item 1",
                     Status = new StatusInfo
                     {
                         Id = 2,
-                        Name = "Bar"
+                        Name = "Status 1"
+                    }
+                },
+                new ToDoItem
+                {
+                    Id = 2,
+                    Name = "Item 2",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 2"
                     }
                 }
             };
-            _mockService
+            _toDoServiceMock
                 .Setup(service => service.GetAllToDoItems())
-                .Returns(stubList.ToList);
-            var controller = new ToDoController(_mockService.Object);
-            //act
-            var result = controller.GetAllToDoItems();
-            //assert
+                .Returns(expectedToDoList.ToList);
+            //Act
+            var result = _controller.GetAllToDoItems();
+            //Assert
             Assert.IsAssignableFrom<IEnumerable<ToDoItem>>(result);
             Assert.NotNull(result);
-            Assert.Equal(stubList, result);
+            Assert.Equal(expectedToDoList, result);
         }
         [Fact]
-        public void GetCompletedToDoItems_shouldReturnIEnumerableToDoItem()
+        public void GetCompletedToDoItems_ShouldCallServiceMethodAndReturnExpectedList()
         {
-            //arrange
-            var stubList = new[]
+            //Arrange
+            var expectedToDoList = new[]
             {
                 new ToDoItem
                 {
                     Id = 1,
-                    Name = "Foo",
+                    Name = "Item 1",
                     Status = new StatusInfo
                     {
                         Id = 2,
-                        Name = "Bar"
+                        Name = "Status 1"
+                    }
+                },
+                new ToDoItem
+                {
+                    Id = 2,
+                    Name = "Item 2",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 2"
                     }
                 }
             };
-            _mockService
+            _toDoServiceMock
                 .Setup(service => service.GetCompletedToDoItems())
-                .Returns(stubList.ToList);
-            var controller = new ToDoController(_mockService.Object);
-            //act
-            var result = controller.GetCompletedToDoItems();
-            //assert
+                .Returns(expectedToDoList.ToList);
+            //Act
+            var result = _controller.GetCompletedToDoItems();
+            //Assert
             Assert.IsAssignableFrom<IEnumerable<ToDoItem>>(result);
             Assert.NotNull(result);
-            Assert.Equal(stubList, result);
+            Assert.Equal(expectedToDoList, result);
+        }
+        [Fact]
+        public void GetDeletedToDoItems_ShouldCallServiceMethodAndReturnExpectedList()
+        {
+            //Arrange
+            var expectedToDoList = new[]
+            {
+                new ToDoItem
+                {
+                    Id = 1,
+                    Name = "Item 1",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 1"
+                    }
+                },
+                new ToDoItem
+                {
+                    Id = 2,
+                    Name = "Item 2",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 2"
+                    }
+                }
+            };
+            _toDoServiceMock
+                .Setup(service => service.GetDeletedToDoItems())
+                .Returns(expectedToDoList.ToList);
+            //Act
+            var result = _controller.GetDeletedToDoItems();
+            //Assert
+            Assert.IsAssignableFrom<IEnumerable<ToDoItem>>(result);
+            Assert.NotNull(result);
+            Assert.Equal(expectedToDoList, result);
+        }
+        [Fact]
+        public void AddToDoItem_ShouldCallServiceMethodAndReturnExpectedToDoItem()
+        {
+            //Arrange
+            var itemName = "New Item";
+            var expectedToDoItem = new ToDoItem
+                {
+                    Id = 1,
+                    Name = "New Item",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 1"
+                    }
+                };
+            _toDoServiceMock
+                .Setup(service => service.AddToDoItem(itemName))
+                .Returns(expectedToDoItem);
+            //Act
+            var result = _controller.AddToDoItem(itemName);
+            //Assert
+            Assert.IsAssignableFrom<ToDoItem>(result);
+            Assert.NotNull(result);
+            Assert.Equal(expectedToDoItem, result);
+        }
+
+        [Fact]
+        public void ChangeToDoItem_ShouldCallServiceMethodWithCorrectParameters()
+        {
+            // Arrange
+            int id = 1;
+            string name = "New Name";
+
+            // Act
+            _controller.ChangeToDoItem(id, name);
+
+            // Assert
+            _toDoServiceMock.Verify(service => service.ChangeToDoItem(id, name), Times.Once);
+        }
+
+        [Fact]
+        public void RemoveToDoItem_ShouldCallServiceMethodWithCorrectParameter()
+        {
+            // Arrange
+            int id = 1;
+
+            // Act
+            _controller.RemoveToDoItem(id);
+
+            // Assert
+            _toDoServiceMock.Verify(service => service.RemoveToDoItemForGood(id), Times.Once);
+        }
+
+        [Fact]
+        public void CompleteToDoItem_ShouldCallServiceMethodAndReturnExpectedStatusInfo()
+        {
+            // Arrange
+            int id = 1;
+            var expectedStatusInfo = new StatusInfo {
+                Id = 1,
+                Name = "Status 1"
+            };
+
+            _toDoServiceMock
+                .Setup(service => service.CompleteToDoItem(id))
+                .Returns(expectedStatusInfo);
+
+            // Act
+            var result = _controller.CompleteToDoItem(id);
+
+            // Assert
+            Assert.Equal(expectedStatusInfo, result);
+            _toDoServiceMock.Verify(service => service.CompleteToDoItem(id), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteToDoItem_ShouldCallServiceMethodAndReturnExpectedStatusInfo()
+        {
+            // Arrange
+            int id = 1;
+            var expectedStatusInfo = new StatusInfo
+            {
+                Id = 1,
+                Name = "Status 1",
+            };
+
+            _toDoServiceMock
+                .Setup(service => service.DeleteToDoItem(id))
+                .Returns(expectedStatusInfo);
+
+            // Act
+            var result = _controller.DeleteToDoItem(id);
+
+            // Assert
+            Assert.Equal(expectedStatusInfo, result);
+            _toDoServiceMock.Verify(service => service.DeleteToDoItem(id), Times.Once);
         }
     }
 }
---
+++ ToDoListManagement.Tests/Services/ToDoServiceTests.cs
@@ -0,0 +1,246 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using toDoListManagement.Models;
+using toDoListManagement.Repositories.Contracts;
+using toDoListManagement.Services;
+
+namespace ToDoListManagement.Tests.Services
+{
+    public class ToDoServiceTests
+    {
+        private ToDoService _toDoService;
+        private Mock<IToDoRepository> _toDoRepositoryMock;
+
+        public ToDoServiceTests()
+        {
+            _toDoRepositoryMock = new Mock<IToDoRepository>();
+            _toDoService = new ToDoService(_toDoRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void GetAllToDoItems_ShouldCallRepositoryMethodAndReturnExpectedList()
+        {
+            // Arrange
+            var expectedToDoList = new[]
+            {
+                new ToDoItem
+                {
+                    Id = 1,
+                    Name = "Item 1",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 1"
+                    }
+                },
+                new ToDoItem
+                {
+                    Id = 2,
+                    Name = "Item 2",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 2"
+                    }
+                }
+            };
+
+            _toDoRepositoryMock
+                .Setup(service => service.GetToDoItems(0))
+                .Returns(expectedToDoList.ToList);
+            
+            // Act
+            var result = _toDoService.GetAllToDoItems();
+
+            // Assert
+            Assert.Equal(expectedToDoList, result);
+            _toDoRepositoryMock.Verify(repository => repository.GetToDoItems(0), Times.Once);
+        }
+
+        [Fact]
+        public void AddToDoItem_ShouldCallRepositoryMethodAndReturnExpectedToDoItem()
+        {
+            // Arrange
+            string itemName = "New Item";
+            var expectedToDoItem = new ToDoItem
+            {
+                Id = 1,
+                Name = "New Item",
+                Status = new StatusInfo
+                {
+                    Id = 2,
+                    Name = "Status 1"
+                }
+            };
+
+            _toDoRepositoryMock
+                .Setup(repository => repository.AddToDoItem(itemName))
+                .Returns(expectedToDoItem);
+
+            // Act
+            var result = _toDoService.AddToDoItem(itemName);
+
+            // Assert
+            Assert.Equal(expectedToDoItem, result);
+            _toDoRepositoryMock.Verify(repository => repository.AddToDoItem(itemName), Times.Once);
+        }
+
+        [Fact]
+        public void ChangeToDoItem_ShouldCallRepositoryMethodWithCorrectParameters()
+        {
+            // Arrange
+            int itemId = 1;
+            string newName = "Updated Name";
+
+            // Act
+            _toDoService.ChangeToDoItem(itemId, newName);
+
+            // Assert
+            _toDoRepositoryMock.Verify(repository => repository.ChangeToDoItem(itemId, newName), Times.Once);
+        }
+
+        [Fact]
+        public void GetCompletedToDoItems_ShouldCallRepositoryMethodAndReturnExpectedList()
+        {
+            // Arrange
+            var expectedToDoList = new[]
+            {
+                new ToDoItem
+                {
+                    Id = 1,
+                    Name = "Item 1",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 1"
+                    }
+                },
+                new ToDoItem
+                {
+                    Id = 2,
+                    Name = "Item 2",
+                    Status = new StatusInfo
+                    {
+                        Id = 2,
+                        Name = "Status 2"
+                    }
+                }
+            };
+
+            _toDoRepositoryMock
+                .Setup(repository => repository.GetToDoItems(2))
+                .Returns(expectedToDoList.ToList);
+
+            // Act
+            var result = _toDoService.GetCompletedToDoItems();
+
+            // Assert
+            Assert.Equal(expectedToDoList, result);
+            _toDoRepositoryMock.Verify(repository => repository.GetToDoItems(2), Times.Once);
+        }
+
+        [Fact]
+        public void GetDeletedToDoItems_ShouldCallRepositoryMethodAndReturnExpectedList()
+        {
+            // Arrange
+            var expectedToDoList = new[]
+            {
+                new ToDoItem
+                {
+                    Id = 1,
+                    Name = "Item 1",
+                    Status = new StatusInfo
+                    {
+                        Id = 3,
+                        Name = "Status 1"
+                    }
+                },
+                new ToDoItem
+                {
+                    Id = 2,
+                    Name = "Item 2",
+                    Status = new StatusInfo
+                    {
+                        Id = 3,
+                        Name = "Status 2"
+                    }
+                }
+            };
+
+            _toDoRepositoryMock
+                .Setup(repository => repository.GetToDoItems(3))
+                .Returns(expectedToDoList.ToList);
+
+            // Act
+            var result = _toDoService.GetDeletedToDoItems();
+
+            // Assert
+            Assert.Equal(expectedToDoList, result);
+            _toDoRepositoryMock.Verify(repository => repository.GetToDoItems(3), Times.Once);
+        }
+
+        [Fact]
+        public void RemoveToDoItem_ShouldCallRepositoryMethodWithCorrectParameter()
+        {
+            // Arrange
+            int id = 1;
+
+            // Act
+            _toDoService.RemoveToDoItemForGood(id);
+
+            // Assert
+            _toDoRepositoryMock.Verify(repository => repository.RemoveToDoItemForGood(id), Times.Once);
+        }
+
+        [Fact]
+        public void CompleteToDoItem_ShouldCallRepositoryMethodAndReturnExpectedStatusInfo()
+        {
+            // Arrange
+            int id = 1;
+            var expectedStatusInfo = new StatusInfo
+            {
+                Id = 1,
+                Name = "Status 1"
+            };
+
+            _toDoRepositoryMock
+                .Setup(repository => repository.CompleteToDoItem(id))
+                .Returns(expectedStatusInfo);
+
+            // Act
+            var result = _toDoService.CompleteToDoItem(id);
+
+            // Assert
+            Assert.Equal(expectedStatusInfo, result);
+            _toDoRepositoryMock.Verify(repository => repository.CompleteToDoItem(id), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteToDoItem_ShouldCallRepositoryMethodAndReturnExpectedStatusInfo()
+        {
+            // Arrange
+            int id = 1;
+            var expectedStatusInfo = new StatusInfo
+            {
+                Id = 1,
+                Name = "Status 1",
+            };
+
+            _toDoRepositoryMock
+                .Setup(repository => repository.DeleteToDoItem(id))
+                .Returns(expectedStatusInfo);
+
+            // Act
+            var result = _toDoService.DeleteToDoItem(id);
+
+            // Assert
+            Assert.Equal(expectedStatusInfo, result);
+            _toDoRepositoryMock.Verify(repository => repository.DeleteToDoItem(id), Times.Once);
+        }
+    }
+}
